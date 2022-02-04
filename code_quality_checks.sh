63e5ae9abbd8fc038df5dd3028d90a8b24bce0a7
Gregor Betz <gregor.betz@kit.edu>
2022-02-04 15:19:50 +0100

    Create code_quality_checks.sh

---
+++ code_quality_checks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Running pylint..."
+poetry run pylint deepa2datasets tests
+
+echo "Running flake8..."
+poetry run flake8 deepa2datasets tests
+
+echo "Running mypy..."
+poetry run mypy deepa2datasets tests
+
+echo "Running black..."
+poetry run black --diff --color deepa2datasets tests
+
